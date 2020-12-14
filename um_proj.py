41afd1eba15d4c0969098bf5dfb905e7a431a5c2
Jakub Chmiel <kuba.chmiel2@gmail.com>
2020-12-14 22:41:41 +0100

    final datasets

--- um_proj.py
+++ um_proj.py
@@ -6,9 +6,11 @@ from sklearn.linear_model import PassiveAggressiveClassifier
 from sklearn.neighbors import NearestNeighbors, KNeighborsClassifier
 from sklearn.metrics import confusion_matrix
 from sklearn.neural_network import MLPClassifier
-from sklearn.naive_bayes import ComplementNB
+from sklearn.naive_bayes import GaussianNB
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.base import clone
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.preprocessing import LabelEncoder
 from scipy.stats import ttest_rel
 import numpy as np
 import sys
@@ -16,48 +18,60 @@ from sklearn.svm import SVR
 from numpy import genfromtxt
 import arff
 from os import listdir
+from xlwt import Workbook
+import math
+from datetime import datetime
 
 alpha = .05
 vThreshold = 0.8
+wb = Workbook()
 
 datasets_files = listdir('datasets')
 
 feature_selection_methods = [
     { 'number': '1', 'name': 'SelectKBest' },
     { 'number': '2', 'name': 'VarianceThreshold' },
-    { 'number': '3', 'name': 'RFE' },
+    { 'number': '3', 'name': 'SelectFromModel' },
 ]
 
 classificators = [
     { 'number': '1', 'clf': KNeighborsClassifier(n_neighbors=10, metric='manhattan'), 'name': 'KNeighborsClassifier' },
     { 'number': '2', 'clf': PassiveAggressiveClassifier(random_state=50), 'name': 'PassiveAggressiveClassifier' },
-    { 'number': '3', 'clf': MLPClassifier(random_state=100), 'name': 'MLPClassifier' },
-    { 'number': '4', 'clf': ComplementNB(), 'name': 'ComplementNB' },
+    { 'number': '3', 'clf': MLPClassifier(random_state=100, max_iter=5000), 'name': 'MLPClassifier' },
+    { 'number': '4', 'clf': GaussianNB(), 'name': 'GaussianNB' },
 ]
 
 def read_file(filename):
     path = 'datasets/' + filename
     dataset = arff.load(open(path, 'r'))
     data = np.transpose(np.array(dataset['data']))
+    le = LabelEncoder()
+    for (i, elem) in enumerate(data):
+        data[i] = le.fit_transform(elem)
+    data = np.array(data, np.int32)
     features = data[:-1]
     classes = data[-1]
-    return (features, classes)
+    return (np.transpose(features), classes)
 
-def get_features(X, y):
-    if feature_selection_method == '1':
+def get_features(X, y, fsm):
+    if fsm == '1':
         # SelectKBest
-        feature_scores = f_selection.SelectKBest(chi2, k=len(X)/2).fit(X, y)
+        kBest = math.ceil(len(X[0])/2)
+        feature_scores = f_selection.SelectKBest(chi2, k=kBest).fit_transform(X, y)
         return feature_scores
-    elif feature_selection_method == '2':
+    elif fsm == '2':
         # VarianceThreshold
         feature_scores = f_selection.VarianceThreshold(threshold=vThreshold).fit_transform(X)
         return feature_scores
-    elif feature_selection_method == '3':
-        # RFE
-        estimator = SVR(kernel="linear")
-        feature_scores = f_selection.RFE(estimator, step=1).fit_transform(X, y)
+    elif fsm == '3':
+        # SelectFromModel
+        clf = ExtraTreesClassifier(n_estimators=50)
+        clf = clf.fit(X, y)
+        model = f_selection.SelectFromModel(clf, prefit=True)
+        feature_scores = model.transform(X)
         return feature_scores
-
+    else:
+        raise ValueError("invalid fsm")
 def get_score(data_train, classes_train, data_test, classes_test, clf):
     clf.fit(data_train, classes_train)
     predicted = clf.predict(data_test)
@@ -65,35 +79,42 @@ def get_score(data_train, classes_train, data_test, classes_test, clf):
 
 
 for (id, filename) in enumerate(datasets_files):
-    print(filename)
+    print('\nFile ' + filename +  ' (' + str(id + 1) + '/' + str(len(datasets_files)) + ')')
+    sheet = wb.add_sheet(filename, cell_overwrite_ok=True)
     data, classes = read_file(filename)
-    features = get_features(data, classes)
+    for (fsm_id, fsm) in enumerate(feature_selection_methods):
+        print('\tFSM ' + fsm['name'] + ' (' + fsm['number'] + '/' + str(len(feature_selection_methods)) + ')')
+        features = get_features(data, classes, fsm['number'])
+        print('\tselected ' + str(len(features[0])) + ' features')
+        
+        fsm_sheet_position = fsm_id * len(classificators) * 3
+        sheet.write(fsm_sheet_position, 0, fsm['name'])
 
-    rskf = RepeatedStratifiedKFold(n_splits=2, n_repeats=5, random_state=25)
-    results = np.zeros((len(selected_classificators), 10))
-####
-    for fold, (train, test) in enumerate(rskf.split(features, classes)):
-        data_train, data_test = features[train], features[test]
-        classes_train, classes_test = classes[train], classes[test]
+        rskf = RepeatedStratifiedKFold(n_splits=2, n_repeats=5, random_state=25)
+        results = np.zeros((len(classificators), 10))
+        
+        for fold, (train, test) in enumerate(rskf.split(features, classes)):
+            data_train, data_test = features[train], features[test]
+            classes_train, classes_test = classes[train], classes[test]
 
-        for i in range(len(selected_classificators)):
-            results[i, fold] = get_score(data_train, classes_train, data_test, classes_test, selected_classificators[i]['clf'])
+            for (clf_id, clf) in enumerate(classificators):
+                results[clf_id, fold] = get_score(data_train, classes_train, data_test, classes_test, clf['clf'])
+                sheet.write(1 + fsm_sheet_position + clf_id * 2, 0, clf['name'])
+                sheet.write(2 + fsm_sheet_position + clf_id * 2, fold, results[clf_id, fold])
 
-    for i in range(len(selected_classificators)):
-        print 'Scores for', selected_classificators[i]['name']
-        print results[i]
-        print
-
-    np.savetxt('results.csv', np.asarray(results), delimiter='\t')
-    test = ttest_rel(results[0], results[1])
-    pval = test.pvalue
-    T = test.statistic
-    print 'p =', pval
-    print 'T =', T
-    print
-    if pval > alpha:
-    	print('Result undetermined')
-    elif T > 0:
-    	print selected_classificators[0]['name'], 'is better'
-    else:
-    	print selected_classificators[1]['name'], 'is better'
+print('\nsaving...')
+date_string = datetime.now().strftime("%Y%m%d_%H%M%S")
+wb.save('results_' + date_string + '.xls')
+print('Done!')
+#        test = ttest_rel(results[0], results[1])
+#        pval = test.pvalue
+#        T = test.statistic
+#        print 'p =', pval
+#        print 'T =', T
+#        print
+#        if pval > alpha:
+#            print('Result undetermined')
+#        elif T > 0:
+#            print selected_classificators[0]['name'], 'is better'
+#        else:
+#            print selected_classificators[1]['name'], 'is better'
